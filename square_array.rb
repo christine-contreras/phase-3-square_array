62c2f799ae6e377f9320e332ad79461f28f00a8a
Christine Contreras <christineec23@yahoo.com>
2021-08-09 17:05:01 -0500

    commit tree a67611358a4bf91baca6c979d98e5ff5ec012bec

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,14 @@
 def square_array(array)
   # your code here
   squared_array = []
-  
+
   array.each do |num|
     squared_array.push(num ** 2)
   end
 
   squared_array
 end
+
+# def squared_array(array)
+#   array.collect{ |num| num ** 2 }
+# end
